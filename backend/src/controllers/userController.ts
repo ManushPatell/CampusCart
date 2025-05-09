72ad4a804af04b1c9354b05d96ab472caaac1090
AndrewI26 <andrewi44@icloud.com>
2025-05-09 19:44:47 -0400

    post new user function

--- backend/src/controllers/userController.ts
+++ backend/src/controllers/userController.ts
@@ -5,12 +5,14 @@ import { findAllUsers, findUserById, addUser } from '../models/userModel.ts';
 export async function getUserById(req: Request, res: Response) {
     const id: number = parseInt(req.params.id);
     if (!id) {
-        return res.status(400).json({ error: 'Id is required' });
+        res.status(400).json({ error: 'Id is required' });
+        return;
     }
 
     const user = await findUserById(id);
     if (!user) {
-        return res.status(500).json({ error: 'User not found!' });
+        res.status(500).json({ error: 'User not found!' });
+        return;
     }
     res.status(200).json(user);
 }
@@ -23,16 +25,19 @@ export async function getAllUsers(req: Request, res: Response) {
 export async function postNewUser(req: Request, res: Response) {
     const { firstName, lastName, email, phoneNumber, password } = req.body;
     if (!firstName || !lastName || !email || !phoneNumber) {
-        return res
-            .status(400)
-            .json({ error: 'Must provide a first name, last name, email and phone number.' });
+        res.status(400).json({
+            error: 'Must provide a first name, last name, email and phone number.',
+        });
+        return;
     }
 
     const saltRounds = 10;
     bcrypt.hash(password, saltRounds, async (err, hash) => {
         if (err) {
-            return res.status(500).json({ error: 'Failed to encrypt password.' });
+            res.status(500).json({ error: 'Failed to encrypt password.' });
+            return;
         }
         const newUser = await addUser(firstName, lastName, email, phoneNumber, hash);
+        res.status(201).json(newUser);
     });
 }
