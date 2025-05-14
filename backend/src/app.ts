41c93cca3bd0e80d3345b533f54f00209a5a1530
AndrewI26 <andrewi44@icloud.com>
2025-05-14 19:21:58 -0400

    docker images to run

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -8,7 +8,8 @@ import userRoutes from './routes/userRoutes.ts';
 import rentalRoutes from './routes/rentalRoutes.ts';
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT;
+const NODE_ENV = process.env.NODE_ENV;
 
 app.use(express.json({ limit: '10mb' }));
 app.use(bodyParser.json());
@@ -24,6 +25,6 @@ app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.listen(PORT, () => {
-    console.log('Server running at http://localhost:3000');
+    console.log(`Server running for ${NODE_ENV} at http://localhost:${PORT}`);
     console.log('JsDoc running on http://localhost:3000/docs');
 });
